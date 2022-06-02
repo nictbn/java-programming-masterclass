83a551c644e33fb2ac605fa9aa8b77b66a10a5b2
Nicu Taban <nictbndeveloper@gmail.com>
2022-06-02 21:24:55 +0300

    Integer Variable Example

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Nick");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
     }
 }

42124fe411edb39707622ab077a365029dc5a219
Nicu Taban <nictbndeveloper@gmail.com>
2022-06-04 11:30:51 +0300

    Operator Precedence challenge

--- Operators/src/academy/learnprogramming/Main.java
+++ Operators/src/academy/learnprogramming/Main.java
@@ -77,5 +77,17 @@ public class Main {
         if (wasCar) {
             System.out.println("wasCar is true");
         }
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("IsNoRemainder = " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
     }
 }

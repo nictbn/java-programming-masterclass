e210fa44f3ccf9ae47b8050602722535b17eea91
Nicu Taban <nictbndeveloper@gmail.com>
2022-06-04 09:53:18 +0300

    Floating Point Precision Challenge

--- FloatAndDouble/src/academy/learnprogramming/Main.java
+++ FloatAndDouble/src/academy/learnprogramming/Main.java
@@ -12,8 +12,20 @@ public class Main {
         System.out.println("Double Minimum Value = " + myMinDoubleValue);
         System.out.println("Double Maximum Value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+        double numberOfPounds = 200d;
+        double kilograms = numberOfPounds * 0.45359237d;
+        System.out.println("Converted kilograms = " + kilograms);
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
     }
 }

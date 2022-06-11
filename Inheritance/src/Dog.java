452f1c29ce7c0e8970f7be4ecb64a5a6fabb0c42
Nicu Taban <nictbndeveloper@gmail.com>
2022-06-11 18:52:06 +0300

    More Inheritance

--- Inheritance/src/Dog.java
+++ Inheritance/src/Dog.java
@@ -29,6 +29,27 @@ public class Dog extends Animal {
         System.out.println("Dog.chew() called");
     }
 
+    public void walk() {
+        System.out.println("Dog.walk() called");
+        super.move(5);
+    }
+
+    public void run() {
+        System.out.println("Dog.run() called");
+        move(10);
+    }
+
+    @Override
+    public void move(int speed) {
+        System.out.println("Dog.move() called");
+        moveLegs(speed);
+        super.move(speed);
+    }
+
+    private void moveLegs(int speed) {
+        System.out.println("Dog.moveLegs() called");
+    }
+
     @Override
     public void eat() {
         System.out.println("Dog.eat() called");

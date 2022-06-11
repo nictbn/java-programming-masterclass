9ab7a3433514588e1d4f237ab158219e5fab42cf
Nicu Taban <nictbndeveloper@gmail.com>
2022-06-11 18:07:42 +0300

    More Coding Exercises

---
+++ CodingExercises/src/ComplexNumber.java
@@ -0,0 +1,31 @@
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary;
+    }
+    public void add(double real, double imaginary) {
+        add(new ComplexNumber(real, imaginary));
+    }
+    public void add(ComplexNumber cn) {
+        this.real += cn.real;
+        this.imaginary += cn.imaginary;
+    }
+    public void subtract(double real, double imaginary) {
+        subtract(new ComplexNumber(real, imaginary));
+    }
+    public void subtract(ComplexNumber cn) {
+        this.real -= cn.real;
+        this.imaginary -= cn.imaginary;
+    }
+}

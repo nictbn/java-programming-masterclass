99797f9b1040695c60b45345333f54e94c729d46
Nicu Taban <nictbndeveloper@gmail.com>
2022-06-14 21:18:54 +0300

    Initial Implementation of the GroceryList

---
+++ ReverseArrayChallenge/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 3, 7, 11, 9, 15};
+        System.out.println("Array = " + Arrays.toString(array));
+        reverse(array);
+        System.out.println("Array = " + Arrays.toString(array));
+    }
+
+    public static void reverse(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+    }
+}

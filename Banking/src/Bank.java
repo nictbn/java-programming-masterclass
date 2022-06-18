bea549dd2ef7da6c2a378758c4c3341694af852d
Nicu Taban <nictbndeveloper@gmail.com>
2022-06-18 10:40:17 +0300

    Fix Bank Challenge

--- Banking/src/Bank.java
+++ Banking/src/Bank.java
@@ -36,16 +36,16 @@ public class Bank {
     public boolean listCustomers(String branchName, boolean showTransactions) {
         Branch branch = findBranch(branchName);
         if (branch != null) {
-            System.out.println("Customers details for branch " + branch.getName());
+            System.out.println("Customer details for branch " + branch.getName());
             ArrayList<Customer> branchCustomers = branch.getCustomers();
             for (int i = 0; i < branchCustomers.size(); i++) {
                 Customer branchCustomer = branchCustomers.get(i);
-                System.out.println("Customer: " + branchCustomer.getName() + "[" + i + "]");
+                System.out.println("Customer: " + branchCustomer.getName() + "[" + (i + 1) + "]");
                 if (showTransactions) {
                     System.out.println("Transactions");
                     ArrayList<Double> transactions = branchCustomer.getTransactions();
                     for (int j = 0; j < transactions.size(); j++) {
-                        System.out.println("[" + (j + 1) + "] Amount " + transactions.get(j));
+                        System.out.println("[" + (j + 1) + "]  Amount " + transactions.get(j));
                     }
                 }
             }

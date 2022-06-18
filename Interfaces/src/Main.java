e3206fe9688da1a115d9eae37bcbfb1196229336
Nicu Taban <nictbndeveloper@gmail.com>
2022-06-18 14:56:20 +0300

    More on interfaces

--- Interfaces/src/Main.java
+++ Interfaces/src/Main.java
@@ -4,5 +4,10 @@ public class Main {
         timsPhone.powerOn();
         timsPhone.callPhone(123456);
         timsPhone.answer();
+
+        timsPhone = new MobilePhone(24565);
+        timsPhone.powerOn();
+        timsPhone.callPhone(24565);
+        timsPhone.answer();
     }
 }

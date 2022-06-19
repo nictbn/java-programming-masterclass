88ccaa4c51e2dc0475675ed395872c594aff1301
Nicu Taban <nictbndeveloper@gmail.com>
2022-06-19 18:14:14 +0300

    Adds the remove operation

--- AbstractionClassesChallenge/src/MyLinkedList.java
+++ AbstractionClassesChallenge/src/MyLinkedList.java
@@ -24,24 +24,20 @@ public class MyLinkedList implements NodeList {
                 if (currentItem.next() != null) {
                     currentItem = currentItem.next();
                 } else {
-                    currentItem.setNext(newItem);
-                    newItem.setPrevious(currentItem);
+                    currentItem.setNext(newItem).setPrevious(currentItem);
                     return true;
                 }
             } else if (comparison > 0) {
                 if (currentItem.previous() != null) {
-                    currentItem.previous().setNext(newItem);
-                    newItem.setPrevious(currentItem.previous());
-                    newItem.setNext(currentItem);
-                    currentItem.setPrevious(newItem);
+                    currentItem.previous().setNext(newItem).setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem).setPrevious(newItem);
                 } else {
-                    newItem.setNext(root);
-                    root.setPrevious(newItem);
+                    newItem.setNext(root).setPrevious(newItem);
                     root = newItem;
                 }
                 return true;
             } else {
-                System.out.println(newItem. getValue() + " os a;ready present, not added");
+                System.out.println(newItem. getValue() + " is already present, not added");
                 return false;
             }
         }
@@ -50,11 +46,40 @@ public class MyLinkedList implements NodeList {
 
     @Override
     public boolean removeItem(ListItem item) {
+        if (item != null) {
+            System.out.println("Deleting item " + item.getValue());
+        }
+        ListItem currentItem = root;
+        while (currentItem != null) {
+            int comparison = currentItem.compareTo(item);
+            if (comparison == 0) {
+                if (currentItem == root) {
+                    root = currentItem.next();
+                } else {
+                    currentItem.previous().setNext(currentItem.next());
+                    if (currentItem.next() != null) {
+                        currentItem.next().setPrevious(currentItem.previous());
+                    }
+                }
+                return true;
+            } else if (comparison < 0) {
+                currentItem = currentItem.next();
+            } else {
+                return false;
+            }
+        }
         return false;
     }
 
     @Override
     public void traverse(ListItem root) {
-
+        if (root == null) {
+            System.out.println("The list is empty");
+        } else {
+            while (root != null) {
+                System.out.println(root.getValue());
+                root = root.next();
+            }
+        }
     }
 }

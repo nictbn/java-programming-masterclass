29d383cfba890f75f8739a286f07d7fe97294307
Nicu Taban <nictbndeveloper@gmail.com>
2022-06-19 17:32:22 +0300

    Initial Implementation pf the Abstract Class Challenge

---
+++ AbstractionClassesChallenge/src/MyLinkedList.java
@@ -0,0 +1,60 @@
+public class MyLinkedList implements NodeList {
+
+    private ListItem root = null;
+
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return root;
+    }
+
+    @Override
+    public boolean addItem(ListItem newItem) {
+        if (root == null) {
+            root = newItem;
+            return true;
+        }
+        ListItem currentItem = root;
+        while (currentItem != null) {
+            int comparison = currentItem.compareTo(newItem);
+            if (comparison < 0) {
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    currentItem.setNext(newItem);
+                    newItem.setPrevious(currentItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                if (currentItem.previous() != null) {
+                    currentItem.previous().setNext(newItem);
+                    newItem.setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem);
+                    currentItem.setPrevious(newItem);
+                } else {
+                    newItem.setNext(root);
+                    root.setPrevious(newItem);
+                    root = newItem;
+                }
+                return true;
+            } else {
+                System.out.println(newItem. getValue() + " os a;ready present, not added");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+
+    }
+}

1149c2472e64b125edb2d10fd6e52418d6545e86
Nicu Taban <nictbndeveloper@gmail.com>
2022-07-03 00:24:34 +0300

    Fix for location

--- Adventure/src/Location.java
+++ Adventure/src/Location.java
@@ -9,7 +9,11 @@ public class Location {
     public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<>(exits);
+        if (exits != null) {
+            this.exits = new HashMap<>(exits);
+        } else {
+            this.exits = new HashMap<>();
+        }
         this.exits.put("Q", 0);
     }
 

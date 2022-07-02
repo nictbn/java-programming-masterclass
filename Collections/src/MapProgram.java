d16857ba724df5c48c62a871d3389ea04a571874
Nicu Taban <nictbndeveloper@gmail.com>
2022-07-02 22:41:44 +0300

    Map Example

---
+++ Collections/src/MapProgram.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProgram {
+    public static void main(String[] args) {
+        Map<String, String> languages = new HashMap<>();
+
+        if (languages.containsKey("Java")) {
+            System.out.println("Java already exists");
+        } else {
+            languages.put("Java", "This course is about Java");
+            System.out.println("Java added successfully");
+        }
+
+        languages.put("Java", "A compiled, high-level, object-oriented, platform independent language");
+        languages.put("Python", "An interpreted, object-oriented, high-level programming language with dynamic semantics");
+        languages.put("Algol", "An algorithmic language");
+        languages.put("BASIC", "Beginners, ALl Purposes Symbolic Instruction Code");
+        languages.put("LISP", "Therein lies madness");
+
+        if (languages.containsKey("Java")) {
+            System.out.println("Java is already in the map");
+        } else {
+            languages.put("Java", "This course is about Java");
+        }
+        System.out.println(languages.get("Java"));
+
+        System.out.println("======================");
+        for (String key: languages.keySet()) {
+            System.out.println(key + " : " + languages.get(key));
+        }
+    }
+}

7c875ebae5ae3778d6fb8ed92de425ff55a63e81
Nicu Taban <nictbndeveloper@gmail.com>
2022-07-02 21:08:53 +0300

    Using the Collections methods

--- Collections/src/Main.java
+++ Collections/src/Main.java
@@ -1,17 +1,61 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Theater theater = new Theater("Olympian", 8, 12);
-//        theater.getSeats();
-        if (theater.reserveSeat("H11")) {
-            System.out.println("Please pay");
+        List<Theater.Seat> seatCopy = new ArrayList<>(theater.seats);
+        printList(seatCopy);
+
+        seatCopy.get(1).reserve();
+        if (theater.reserveSeat("A02")) {
+            System.out.println("Please pay for A02");
         } else {
-            System.out.println("Sorry, seat is taken");
+            System.out.println("Seat already reserved");
         }
 
-        if (theater.reserveSeat("H11")) {
-            System.out.println("Please pay");
-        } else {
-            System.out.println("Sorry, seat is taken");
+        Collections.reverse(seatCopy);
+        System.out.println("Printing seatCopy");
+        printList(seatCopy);
+        System.out.println("Printing theater.seats");
+        printList(theater.seats);
+
+        Collections.shuffle(seatCopy);
+        System.out.println("Printing shuffled seatCoyp");
+        printList(seatCopy);
+        System.out.println("Printing shuffled theater.seats");
+        printList(theater.seats);
+
+        Theater.Seat minSeat = Collections.min(seatCopy);
+        Theater.Seat maxSeat = Collections.max(seatCopy);
+        System.out.println("Min seat number is " + minSeat.getSeatNumber());
+        System.out.println("Max seat number is " + maxSeat.getSeatNumber());
+
+        sortList(seatCopy);
+        System.out.println("Printing sorted seatCopy");
+        printList(seatCopy);
+
+//        The two lines below do not work because the new list does not contain allocated seats (it has no references to seat objects)
+//        List<Theater.Seat> newList = new ArrayList<>(theater.seats.size());
+//        Collections.copy(newList, theater.seats);
+    }
+
+    public static void printList(List<Theater.Seat> list) {
+        for (Theater.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+        System.out.println();
+        System.out.println("==========================================");
+    }
+
+    public static void sortList(List<? extends Theater.Seat> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    Collections.swap(list, i, j);
+                }
+            }
         }
     }
 }
--- Collections/src/Theater.java
+++ Collections/src/Theater.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Theater {
     private final String theaterName;
-    private List<Seat> seats = new ArrayList<>();
+    public List<Seat> seats = new ArrayList<>();
 
     public String getTheaterName() {
         return theaterName;
@@ -37,7 +37,7 @@ public class Theater {
         }
     }
 
-    private class Seat implements Comparable<Seat> {
+    public class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
 

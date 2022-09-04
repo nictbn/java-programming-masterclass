7020c98d763bc7e13c533ffc195feb1adeeb1ded
Nicu Taban <nictbndeveloper@gmail.com>
2022-09-04 20:00:45 +0300

    Using a pipe to send data between threads

--- JavaNio/src/Main.java
+++ JavaNio/src/Main.java
@@ -1,57 +1,61 @@
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
+import java.nio.channels.Pipe;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        try (FileOutputStream binFile = new FileOutputStream("data.dat");
-             FileChannel binChannel = binFile.getChannel();) {
-            ByteBuffer buffer = ByteBuffer.allocate(100);
-            byte[] outputBytes = "Hello World!".getBytes();
-            long int1Pos = outputBytes.length;
-            buffer.put(outputBytes);
-            buffer.putInt(245);
-            long int2pos = int1Pos + Integer.BYTES;
-            buffer.putInt(-98765);
-            byte[] outputBytes2 = "Nice to meet you".getBytes();
-            buffer.put(outputBytes2);
-            long int3Pos = int2pos + Integer.BYTES + outputBytes2.length;
-            buffer.putInt(1000);
-            buffer.flip();
-            binChannel.write(buffer);
+        try {
+            Pipe pipe = Pipe.open();
 
-            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-            FileChannel channel = ra.getChannel();
+            Runnable writer = new Runnable() {
+                @Override
+                public void run() {
+                   try {
+                       Pipe.SinkChannel sinkChannel = pipe.sink();
+                       ByteBuffer buffer = ByteBuffer.allocate(56);
 
-            ByteBuffer readBuffer = ByteBuffer.allocate(Integer.BYTES);
-            channel.position(int3Pos);
-            channel.read(readBuffer);
-            readBuffer.flip();
-            System.out.println("int3 = " + readBuffer.getInt());
+                       for (int i = 0; i < 10; i++) {
+                           String currentTime = "The time is: " + System.currentTimeMillis();
+                           buffer.put(currentTime.getBytes());
+                           buffer.flip();
 
-            readBuffer.flip();
-            channel.position(int2pos);
-            channel.read(readBuffer);
-            readBuffer.flip();
-            System.out.println("int2 = " + readBuffer.getInt());
+                           while (buffer.hasRemaining()) {
+                               sinkChannel.write(buffer);
+                           }
+                           buffer.flip();
+                           Thread.sleep(100);
+                       }
+                   } catch (Exception e) {
+                       e.printStackTrace();
+                   }
+                }
+            };
 
-            readBuffer.flip();
-            channel.position(int1Pos);
-            channel.read(readBuffer);
-            readBuffer.flip();
-            System.out.println("int1 = " + readBuffer.getInt());
+            Runnable reader = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Pipe.SourceChannel sourceChannel = pipe.source();
+                        ByteBuffer buffer = ByteBuffer.allocate(56);
 
-            RandomAccessFile copyFile = new RandomAccessFile("datacopy.dat", "rw");
-            FileChannel copyChannel = copyFile.getChannel();
-            channel.position(0);
-//            long numTransferred = copyChannel.transferFrom(channel, 0, channel.size());
-            long numTransferred = channel.transferTo(0, channel.size(), copyChannel);
-            System.out.println("Num transferred = " + numTransferred);
-            channel.close();
-            ra.close();
-            copyChannel.close();
+                        for (int i = 0; i < 10; i++) {
+                            int bytesRead = sourceChannel.read(buffer);
+                            byte[] timeString = new byte[bytesRead];
+                            buffer.flip();
+                            buffer.get(timeString);
+                            System.out.println("Reader Thread: " + new String(timeString));
+                            buffer.flip();
+                            Thread.sleep(100);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            new Thread(writer).start();
+            new Thread(reader).start();
         } catch (IOException e) {
             e.printStackTrace();
         }

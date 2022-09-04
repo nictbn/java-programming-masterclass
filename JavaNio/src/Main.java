4d39b1eda9abb152b754c83e4fe5d05c951403f3
Nicu Taban <nictbndeveloper@gmail.com>
2022-09-04 16:48:40 +0300

    Reading with buffer flips

--- JavaNio/src/Main.java
+++ JavaNio/src/Main.java
@@ -26,13 +26,24 @@ public class Main {
             System.out.println("numBytes written was: " + numBytes);
 
             RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-            byte[] b = new byte[outputBytes.length];
-            ra.read(b);
-            System.out.println(new String(b));
-            long int1 = ra.readInt();
-            long int2 = ra.readInt();
-            System.out.println(int1);
-            System.out.println(int2);
+            FileChannel channel = ra.getChannel();
+            outputBytes[0] = 'a';
+            outputBytes[1] = 'b';
+            buffer.flip();
+            long numBytesRead  = channel.read(buffer);
+            if (buffer.hasArray()) {
+                System.out.println("byte buffer = " + new String(buffer.array()));
+            }
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            intBuffer.flip();
+            System.out.println(intBuffer.getInt());
+            intBuffer.flip();
+            numBytesRead=channel.read(intBuffer);
+            intBuffer.flip();
+            System.out.println(intBuffer.getInt());
+            channel.close();
+            ra.close();
         } catch (IOException e) {
             e.printStackTrace();
         }

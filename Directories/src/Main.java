1bde5c9af73945545a31e3ca950bf0da556a2e4b
Nicu Taban <nictbndeveloper@gmail.com>
2022-09-05 23:53:30 +0300

    Copy Subdirectory Example

--- Directories/src/Main.java
+++ Directories/src/Main.java
@@ -6,10 +6,11 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Walking tree for dir 2 ");
         Path dir2Path = FileSystems.getDefault().getPath("Directories", "FileTree" + File.separator + "Dir2");
+        System.out.println(" Copy Dir2 to Dir4/Dir2Copy ");
+        Path copyPath = FileSystems.getDefault().getPath("Directories" + File.separator + "FileTree" + File.separator + "Dir4" + File.separator + "Dir2Copy");
         try {
-            Files.walkFileTree(dir2Path, new PrintNames());
+            Files.walkFileTree(dir2Path, new CopyFiles(dir2Path, copyPath));
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }

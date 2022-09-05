dca371a32774fccfdfd16e08f52e30fbaf0d8e83
Nicu Taban <nictbndeveloper@gmail.com>
2022-09-05 22:20:34 +0300

    Directory Create Example

--- Paths/src/Main.java
+++ Paths/src/Main.java
@@ -6,8 +6,8 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) {
         try {
-            Path fieToCreate = FileSystems.getDefault().getPath("Paths","Examples", "file3.txt");
-            Files.createFile(fieToCreate);
+            Path dirToCreate = FileSystems.getDefault().getPath("Paths", "Examples", "Dir4");
+            Files.createDirectory(dirToCreate);
         } catch (IOException e) {
             e.printStackTrace();
         }

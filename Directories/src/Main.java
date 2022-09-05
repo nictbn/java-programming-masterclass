53fc8f99249ba1c85cfbb4ed8b61fe53ef41267e
Nicu Taban <nictbndeveloper@gmail.com>
2022-09-05 22:52:57 +0300

    Output .dat files in directory

---
+++ Directories/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Path directory = FileSystems.getDefault().getPath("Directories/FileTree/Dir2");
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, "*.dat")) {
+            for (Path file : contents) {
+                System.out.println(file.getFileName());
+            }
+        } catch (IOException | DirectoryIteratorException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}

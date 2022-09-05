c7579bd679786f7e32b455e0d06987bcdaf18bb1
Nicu Taban <nictbndeveloper@gmail.com>
2022-09-05 22:37:26 +0300

    Basic File Attributes example

--- Paths/src/Main.java
+++ Paths/src/Main.java
@@ -2,12 +2,22 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Path dirToCreate = FileSystems.getDefault().getPath("Paths","Examples", "Dir2/Dir3/Dir4/Dir5/Dir6");
-            Files.createDirectories(dirToCreate);
+            Path filePath = FileSystems.getDefault().getPath("Paths", "Examples", "Dir1/file1.txt");
+            long size = Files.size(filePath);
+            System.out.println("Size = " + size);
+            System.out.println("Last modified = " + Files.getLastModifiedTime(filePath));
+
+            BasicFileAttributes attributes = Files.readAttributes(filePath, BasicFileAttributes.class);
+            System.out.println("Size = " + attributes.size());
+            System.out.println("Last modified = " + attributes.lastModifiedTime());
+            System.out.println("Created = " + attributes.creationTime());
+            System.out.println("Is directory = " + attributes.isDirectory());
+            System.out.println("Is regular file = " + attributes.isRegularFile());
         } catch (IOException e) {
             e.printStackTrace();
         }

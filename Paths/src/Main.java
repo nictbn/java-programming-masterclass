e53052dc2ea95d67ff566d8222fb1a2322469777
Nicu Taban <nictbndeveloper@gmail.com>
2022-09-05 22:23:21 +0300

    Creates nested empty folders

--- Paths/src/Main.java
+++ Paths/src/Main.java
@@ -6,8 +6,8 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) {
         try {
-            Path dirToCreate = FileSystems.getDefault().getPath("Paths", "Examples", "Dir4");
-            Files.createDirectory(dirToCreate);
+            Path dirToCreate = FileSystems.getDefault().getPath("Paths","Examples", "Dir2/Dir3/Dir4/Dir5/Dir6");
+            Files.createDirectories(dirToCreate);
         } catch (IOException e) {
             e.printStackTrace();
         }

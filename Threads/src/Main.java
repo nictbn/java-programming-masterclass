e019dc1d7797bd235ee87bd65ed9b3fe112819ab
Nicu Taban <nictbndeveloper@gmail.com>
2022-09-18 12:05:05 +0300

    Join with timeout example

--- Threads/src/Main.java
+++ Threads/src/Main.java
@@ -17,8 +17,8 @@ public class Main {
             public void run() {
                 System.out.println(ThreadColor.ANSI_RED + "Hello from the anonymous class's implementation of run");
                 try {
-                    anotherThread.join();
-                    System.out.println(ThreadColor.ANSI_RED + "AnotherThread terminated so I'm running again");
+                    anotherThread.join(2000);
+                    System.out.println(ThreadColor.ANSI_RED + "AnotherThread terminated or timed out so I'm running again");
                 } catch (InterruptedException e) {
                     System.out.println(ThreadColor.ANSI_RED + "I couldn't wait after all. I was interrupted");
                 }

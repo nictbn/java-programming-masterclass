17d73359ab301eb54d3b162a7c94832e5f83ff84
Nicu Taban <nictbndeveloper@gmail.com>
2022-09-18 11:44:40 +0300

    Runnable example

--- Threads/src/Main.java
+++ Threads/src/Main.java
@@ -9,6 +9,15 @@ public class Main {
                 System.out.println(ThreadColor.ANSI_GREEN + "Hello from the anonymous class thread");
             }
         }.start();
+        Thread myRunnableThread = new Thread(new MyRunnable());
+        myRunnableThread.start();
+        Thread mySecondRunnableThread = new Thread(new MyRunnable() {
+            @Override
+            public void run() {
+                System.out.println(ThreadColor.ANSI_RED + "Hello from the anonymous class's implementation of run");
+            }
+        });
+        mySecondRunnableThread.start();
         System.out.println(ThreadColor.ANSI_PURPLE + "Hello again from the main thread.");
 //        anotherThread.start(); Starting the thread a second time will throw an exception
     }

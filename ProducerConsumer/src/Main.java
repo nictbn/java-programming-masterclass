2c5cbe6794c9c1b34adf4226ecb3a46cb3ba7292
Nicu Taban <nictbndeveloper@gmail.com>
2022-10-16 14:24:08 +0300

    Locking and unlocking using try finally constructs

--- ProducerConsumer/src/Main.java
+++ ProducerConsumer/src/Main.java
@@ -38,8 +38,11 @@ class MyProducer implements Runnable {
             try {
                 System.out.println(color + "Adding... " + num);
                 bufferLock.lock();
-                buffer.add(num);
-                bufferLock.unlock();
+                try {
+                    buffer.add(num);
+                } finally {
+                    bufferLock.unlock();
+                }
                 Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e) {
                 System.out.println("Producer was interrupted");
@@ -47,8 +50,11 @@ class MyProducer implements Runnable {
         }
         System.out.println(color + "Adding EOF and exiting...");
         bufferLock.lock();
-        buffer.add("EOF");
-        bufferLock.unlock();
+        try {
+            buffer.add("EOF");
+        } finally {
+            bufferLock.unlock();
+        }
     }
 }
 
@@ -67,18 +73,19 @@ class MyConsumer implements Runnable {
     public void run() {
         while (true) {
             bufferLock.lock();
-            if (buffer.isEmpty()) {
+            try {
+                if (buffer.isEmpty()) {
+                    continue;
+                }
+                if (buffer.get(0).equals(Main.EOF)) {
+                    System.out.println(color + "Exiting");
+                    break;
+                } else {
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
+            } finally {
                 bufferLock.unlock();
-                continue;
             }
-            if (buffer.get(0).equals(Main.EOF)) {
-                System.out.println(color + "Exiting");
-                bufferLock.unlock();
-                break;
-            } else {
-                System.out.println(color + "Removed " + buffer.remove(0));
-            }
-            bufferLock.unlock();
         }
     }
 }

8825b67cc2712f3c952e431e1fba82b14a64d13c
Nicu Taban <nictbndeveloper@gmail.com>
2022-10-24 23:40:23 +0300

    Adds progres bar and label and binds them

--- taskExample/src/main/java/com/example/taskexample/Controller.java
+++ taskExample/src/main/java/com/example/taskexample/Controller.java
@@ -1,11 +1,12 @@
 package com.example.taskexample;
 
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 
 public class Controller {
     private Task<ObservableList<String>> task;
@@ -13,23 +14,37 @@ public class Controller {
     @FXML
     private ListView listView;
 
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Label progressLabel;
+
     public void initialize() {
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
-                Thread.sleep(1000);
-                final ObservableList<String> employees = FXCollections.observableArrayList(
+                String[] names = {
                         "Tim Buchalka",
                         "Bill Rogers",
                         "Jack Jill",
                         "Joan Andrews",
                         "Mary Johnson",
                         "Bob McDonald"
-                );
+                };
+                final ObservableList<String> employees = FXCollections.observableArrayList();
+
+                for (int i = 0; i < 6; i++) {
+                    employees.add(names[i]);
+                    updateMessage("Added " + names[i] + " to the list");
+                    updateProgress(i + 1, 6);
+                    Thread.sleep(300);
+                }
                 return employees;
             }
         };
-
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressLabel.textProperty().bind(task.messageProperty());
         listView.itemsProperty().bind(task.valueProperty());
     }
 

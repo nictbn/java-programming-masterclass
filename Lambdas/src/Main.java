02006f03afc41cfc44757a4a1b28c5f52b08bb51
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-05 18:09:33 +0200

    Sort with lambda

--- Lambdas/src/Main.java
+++ Lambdas/src/Main.java
@@ -30,13 +30,15 @@ public class Main {
         employees.add(jack);
         employees.add(snow);
 
-        Collections.sort(employees, new Comparator<Employee>() {
-            @Override
-            public int compare(Employee employee1, Employee employee2) {
-                return employee1.getName().compareTo(employee2.getName());
-            }
-        });
+//        Collections.sort(employees, new Comparator<Employee>() {
+//            @Override
+//            public int compare(Employee employee1, Employee employee2) {
+//                return employee1.getName().compareTo(employee2.getName());
+//            }
+//        });
 
+        Collections.sort(employees, (employee1, employee2) ->
+                employee1.getName().compareTo(employee2.getName()));
         for (Employee employee : employees) {
             System.out.println(employee.getName());
         }

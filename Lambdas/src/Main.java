3067662df1b79c4fc328ded42a74c767dc55cb8b
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-05 19:29:01 +0200

    Lambda is treated as an embedded code block in a class not as an anonymous class

--- Lambdas/src/Main.java
+++ Lambdas/src/Main.java
@@ -93,13 +93,20 @@ interface UpperConcat {
 
 class AnotherClass {
     public String doSomething() {
+        UpperConcat uc = (s1, s2) -> {
+            System.out.println("The lambda expression's class is " + getClass().getSimpleName());
+            String result = s1.toUpperCase() + s2.toUpperCase();
+            return  result;
+        };
         System.out.println("The AnotherClass class's name is: " + getClass().getSimpleName());
-        return Main.doStringStuff(new UpperConcat() {
-            @Override
-            public String upperAndConcat(String s1, String s2) {
-                System.out.println("The anonymous class's name is: " + getClass().getSimpleName());
-                return s1.toUpperCase() + s2.toUpperCase();
-            }
-        }, "String1", "String2");
+        return Main.doStringStuff(uc, "String1", "String2");
+//        System.out.println("The AnotherClass class's name is: " + getClass().getSimpleName());
+//        return Main.doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                System.out.println("The anonymous class's name is: " + getClass().getSimpleName());
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        }, "String1", "String2");
     }
 }

d087fe767ef1f54992e71b1c4d4453ba9fba5c45
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-05 14:54:47 +0200

    First lambda example

--- Lambdas/src/Main.java
+++ Lambdas/src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        new Thread(new CodeToRun()).start();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Printing from the second runnable");
-            }
+//        new Thread(new CodeToRun()).start();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Printing from the second runnable");
+//            }
+//        }).start();
+
+        new Thread(() -> {
+            System.out.println("Printing from the lambda runnable");
+            System.out.println("Line 2");
+            System.out.format("This is line %d\n", 3);
         }).start();
     }
 }

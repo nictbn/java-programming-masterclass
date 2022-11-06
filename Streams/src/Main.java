7a4bda379de9fcfb4933a84acc52d1d6bc2d67aa
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-06 20:51:33 +0200

    FlatMap Example

--- Streams/src/Main.java
+++ Streams/src/Main.java
@@ -33,5 +33,25 @@ public class Main {
         Stream<String> concatStream = Stream.concat(ioNumberStream, inNumberStream);
         System.out.println("-------------------------------");
         System.out.println(concatStream.distinct().peek(System.out::println).count());
+
+        Employee john = new Employee("John Doe", 30);
+        Employee jane = new Employee("Jane Deer", 25);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+
+        Department hr = new Department("Human Resources");
+        hr.addEmployee(jane);
+        hr.addEmployee(jack);
+        hr.addEmployee(snow);
+
+        Department accounting = new Department("Accounting");
+        accounting.addEmployee(john);
+
+        List<Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(accounting);
+
+        departments.stream().flatMap(department -> department.getEmployees().stream())
+                .forEach(System.out::println);
     }
 }

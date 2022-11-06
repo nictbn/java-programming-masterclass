d00d53819ffa563223584002170a2d1e562c504f
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-06 20:55:45 +0200

    Collector Example

--- Streams/src/Main.java
+++ Streams/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -53,5 +54,15 @@ public class Main {
 
         departments.stream().flatMap(department -> department.getEmployees().stream())
                 .forEach(System.out::println);
+
+        List<String> sortedGNumbers = someBingoNumbers.stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(Collectors.toList());
+
+        for (String s : sortedGNumbers) {
+            System.out.println(s);
+        }
     }
 }

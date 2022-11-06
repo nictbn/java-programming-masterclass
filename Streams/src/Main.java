d96380212f78e3fd564885ceb47a9d030afb221b
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-06 19:56:56 +0200

    Filtering with if statement regardless of upper case or lower case

--- Streams/src/Main.java
+++ Streams/src/Main.java
@@ -4,11 +4,15 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<String> someBingoNumbers = Arrays.asList(
-                "N40", "N36", "B40", "B12", "B6", "G53", "G49", "G60", "G50", "I26", "I17", "I29", "O71"
+                "N40", "N36",
+                "B40", "B12", "B6",
+                "G53", "G49", "G60", "G50", "g64",
+                "I26", "I17", "I29",
+                "O71"
         );
 
         someBingoNumbers.forEach(number -> {
-            if (number.startsWith("G")) {
+            if (number.toUpperCase().startsWith("G")) {
                 System.out.println(number);
             }
         });

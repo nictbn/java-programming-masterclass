bd1a133b2f6856f5302b98806d0df2c700cd494a
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-06 21:46:04 +0200

    Lambda Challenge 2

---
+++ LambdaChallenges/src/Challenge2.java
@@ -0,0 +1,23 @@
+import java.util.function.Function;
+
+public class Challenge2 {
+    public static String everySecondChar(String source) {
+        StringBuilder returnVal = new StringBuilder();
+        for (int i = 0; i < source.length(); i++) {
+            if (i % 2 == 1) {
+                returnVal.append(source.charAt(i));
+            }
+        }
+        return returnVal.toString();
+    }
+
+    Function<String, String> lambdaFunction = (String source) -> {
+        StringBuilder returnVal = new StringBuilder();
+        for (int i = 0; i < source.length(); i++) {
+            if (i % 2 == 1) {
+                returnVal.append(source.charAt(i));
+            }
+        }
+        return returnVal.toString();
+    };
+}

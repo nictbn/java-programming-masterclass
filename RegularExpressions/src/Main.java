da0a1bcde5e354b5cdebebb214ed60349ce2ab42
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-12 15:10:24 +0200

    Regex replace all example

---
+++ RegularExpressions/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String string = "I am a string. Yes, I am.";
+        System.out.println(string);
+        String yourString = string.replaceAll("I", "You");
+        System.out.println(yourString);
+    }
+}

f21e338d32b2e0e37129f9ee92c81f363525f85d
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-12 17:04:42 +0200

    Replace a through f A through F and 3 through 8 the short way with construct that ignores case sensitivity

--- RegularExpressions/src/Main.java
+++ RegularExpressions/src/Main.java
@@ -29,5 +29,6 @@ public class Main {
         System.out.println(newAlphaNumeric.replaceAll("[abcdef345678]", "X"));
         System.out.println(newAlphaNumeric.replaceAll("[a-f3-8]", "X"));
         System.out.println(newAlphaNumeric.replaceAll("[a-fA-F3-8]", "X"));
+        System.out.println(newAlphaNumeric.replaceAll("(?i)[a-f3-8]", "X"));
     }
 }

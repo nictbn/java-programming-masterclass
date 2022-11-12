9469cf6405cb7ac541d0522bec6b2b5ca0534c55
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-12 20:53:57 +0200

    String is not changed if e appears only once

--- RegularExpressions/src/Main.java
+++ RegularExpressions/src/Main.java
@@ -50,7 +50,7 @@ public class Main {
         System.out.println(thirdAlphaNumericString.replaceAll("^abcD+", "YYY"));
         System.out.println(thirdAlphaNumericString.replaceAll("^abcD*", "YYY"));
 
-        String fourthAlphaNumericString = "abcDeeeF13Ghhiiijkl99z";
+        String fourthAlphaNumericString = "abcDeF13Ghhiiijkl99z";
         System.out.println(fourthAlphaNumericString.replaceAll("^abcDe{2,5}", "YYY"));
     }
 }

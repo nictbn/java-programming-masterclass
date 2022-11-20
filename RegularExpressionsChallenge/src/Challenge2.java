adb76f79b94789a58e8cbac9767e40cd681965c3
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-20 20:13:49 +0200

    Alternative solution using groups

--- RegularExpressionsChallenge/src/Challenge2.java
+++ RegularExpressionsChallenge/src/Challenge2.java
@@ -1,9 +1,12 @@
 public class Challenge2 {
     public static void main(String[] args) {
-        String challenge1 = "I want a bike";
-        String chalenge2 = "I want a ball";
+        String challenge1 = "I want a bike.";
+        String chalenge2 = "I want a ball.";
         String regExp = "I want a \\w+.";
         System.out.println(challenge1.matches(regExp));
         System.out.println(chalenge2.matches(regExp));
+        String regExp1 = "I want a (bike|ball).";
+        System.out.println(challenge1.matches(regExp1));
+        System.out.println(chalenge2.matches(regExp1));
     }
 }

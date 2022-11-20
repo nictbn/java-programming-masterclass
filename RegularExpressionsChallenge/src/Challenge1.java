462aa26174def93dab395aa5f6d6772bf886ed49
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-20 20:07:25 +0200

    Challenge 1

---
+++ RegularExpressionsChallenge/src/Challenge1.java
@@ -0,0 +1,6 @@
+public class Challenge1 {
+    public static void main(String[] args) {
+        String challenge1 = "I want a bike";
+        System.out.println(challenge1.matches("I want a bike"));
+    }
+}

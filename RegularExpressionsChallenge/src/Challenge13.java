1458738da85b31d060591be337cd2fe94dc842ac
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-20 21:03:05 +0200

    Challenge 13

---
+++ RegularExpressionsChallenge/src/Challenge13.java
@@ -0,0 +1,6 @@
+public class Challenge13 {
+    public static void main(String[] args) {
+        String challenge13 = "11111-1111";
+        System.out.println(challenge13.matches("^\\d{5}-\\d{4}$"));
+    }
+}

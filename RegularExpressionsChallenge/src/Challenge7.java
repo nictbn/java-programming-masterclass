83a235cb5d233a1af88630c2aa894ac28789241b
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-20 20:37:53 +0200

    Challenge 7

---
+++ RegularExpressionsChallenge/src/Challenge7.java
@@ -0,0 +1,6 @@
+public class Challenge7 {
+    public static void main(String[] args) {
+        String challenge7 = "abcd.135";
+        System.out.println(challenge7.matches("^[A-Za-z]+\\.\\d+$"));
+    }
+}

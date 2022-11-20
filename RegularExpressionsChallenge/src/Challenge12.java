32acc8f78c9bc03fdf99ba97e8777d054df4708d
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-20 21:01:27 +0200

    Challenge 12

---
+++ RegularExpressionsChallenge/src/Challenge12.java
@@ -0,0 +1,6 @@
+public class Challenge12 {
+    public static void main(String[] args) {
+        String challenge12 = "11111";
+        System.out.println(challenge12.matches("^\\d{5}$"));
+    }
+}

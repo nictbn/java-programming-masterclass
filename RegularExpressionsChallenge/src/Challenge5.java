3d1b6c09e18646c8e7ef09e3c74bdfc0f15c63c1
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-20 20:29:07 +0200

    Challenge 5

---
+++ RegularExpressionsChallenge/src/Challenge5.java
@@ -0,0 +1,7 @@
+public class Challenge5 {
+    public static void main(String[] args) {
+        String challenge5 = "aaabccccccccdddefffg";
+        System.out.println(challenge5.matches("[abcdefg]+"));
+        System.out.println(challenge5.matches("[a-g]+"));
+    }
+}

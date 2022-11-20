474a36e3c77780b31b685cee7da284e6603d1436
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-20 20:20:36 +0200

    Challenge 4

---
+++ RegularExpressionsChallenge/src/Challenge4.java
@@ -0,0 +1,7 @@
+public class Challenge4 {
+    public static void main(String[] args) {
+        String challenge4 = "Replace all blanks with underscores";
+        System.out.println(challenge4.replaceAll(" ", "_"));
+        System.out.println(challenge4.replaceAll("\\s", "_"));
+    }
+}

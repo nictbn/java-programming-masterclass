8a8338681d764ff0bf8b1aa91d5cda5a0e4ec22f
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-26 16:16:19 +0200

    Using advanced debugging features

--- DebuggerDemo/src/StringUtilities.java
+++ DebuggerDemo/src/StringUtilities.java
@@ -1,7 +1,18 @@
+import java.util.Locale;
+
 public class StringUtilities {
     private int charsAdded = 0;
     public void addChar(StringBuilder stringBuilder, char c) {
         stringBuilder.append(c);
         charsAdded++;
+    }
+
+    public String upperAndPrefix(String str) {
+        String upper = str.toUpperCase();
+        return "Prefix_" + upper;
+    }
+
+    public String addSuffix(String str) {
+        return str + "__Suffix";
     }
 }

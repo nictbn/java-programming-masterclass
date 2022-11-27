56355f8f4e9007fb37f84259163512720c2ccae3
Nicu Taban <nictbndeveloper@gmail.com>
2022-11-27 09:57:10 +0200

    Challenge 1: create stubs

---
+++ JunitChallenges/src/UtilitiesTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilitiesTest {
+
+    @Test
+    void everyNthChar() {
+        fail("Empty test");
+    }
+
+    @Test
+    void removePairs() {
+        fail("Empty test");
+    }
+
+    @Test
+    void converter() {
+        fail("Empty test");
+    }
+
+    @Test
+    void nullIfOddLength() {
+        fail("Empty test");
+    }
+}

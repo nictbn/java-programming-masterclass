67c9d9261a503e9b9361e97952c29d4fbb018c5d
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-26 11:48:39 +0200

    Insert examples

--- TestDB/src/Main.java
+++ TestDB/src/Main.java
@@ -13,7 +13,11 @@ public class Main {
         String conntectionString = "jdbc:sqlite:" + path + File.separator + "TestDB" + File.separator + "testjava.db";
 
         try (Connection conn = DriverManager.getConnection(conntectionString); Statement statement = conn.createStatement();) {
+//            conn.setAutoCommit(false);
             statement.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
+            statement.execute("INSERT INTO contacts(name, phone, email) VALUES('Joe', 45632, 'joe@anywhere.com')");
+            statement.execute("INSERT INTO contacts(name, phone, email) VALUES('Jane', 4829484, 'jane@somewhere.com')");
+            statement.execute("INSERT INTO contacts(name, phone, email) VALUES('Fido', 9038, 'dog@email.com')");
         } catch (SQLException e) {
             System.out.println("Something went wrong: " + e.getMessage());
         }

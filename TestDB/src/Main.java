93cc7db68fc039e65a6a881fb6316221afc3e54e
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-26 11:51:14 +0200

    Update Example

--- TestDB/src/Main.java
+++ TestDB/src/Main.java
@@ -14,10 +14,11 @@ public class Main {
 
         try (Connection conn = DriverManager.getConnection(conntectionString); Statement statement = conn.createStatement();) {
 //            conn.setAutoCommit(false);
-            statement.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
-            statement.execute("INSERT INTO contacts(name, phone, email) VALUES('Joe', 45632, 'joe@anywhere.com')");
-            statement.execute("INSERT INTO contacts(name, phone, email) VALUES('Jane', 4829484, 'jane@somewhere.com')");
-            statement.execute("INSERT INTO contacts(name, phone, email) VALUES('Fido', 9038, 'dog@email.com')");
+//            statement.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
+//            statement.execute("INSERT INTO contacts(name, phone, email) VALUES('Joe', 45632, 'joe@anywhere.com')");
+//            statement.execute("INSERT INTO contacts(name, phone, email) VALUES('Jane', 4829484, 'jane@somewhere.com')");
+//            statement.execute("INSERT INTO contacts(name, phone, email) VALUES('Fido', 9038, 'dog@email.com')");
+            statement.execute("UPDATE contacts SET phone=5566789 WHERE name='Jane'");
         } catch (SQLException e) {
             System.out.println("Something went wrong: " + e.getMessage());
         }

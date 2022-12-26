85a7f1347b19fac6cb41750429beacb7b32ca2e0
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-26 12:00:02 +0200

    Select example

--- TestDB/src/Main.java
+++ TestDB/src/Main.java
@@ -1,10 +1,7 @@
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,8 +15,14 @@ public class Main {
 //            statement.execute("INSERT INTO contacts(name, phone, email) VALUES('Joe', 45632, 'joe@anywhere.com')");
 //            statement.execute("INSERT INTO contacts(name, phone, email) VALUES('Jane', 4829484, 'jane@somewhere.com')");
 //            statement.execute("INSERT INTO contacts(name, phone, email) VALUES('Fido', 9038, 'dog@email.com')");
-            statement.execute("UPDATE contacts SET phone=5566789 WHERE name='Jane'");
-            statement.execute("DELETE FROM contacts WHERE name='Joe'");
+//            statement.execute("UPDATE contacts SET phone=5566789 WHERE name='Jane'");
+//            statement.execute("DELETE FROM contacts WHERE name='Joe'");
+            statement.execute("SELECT * FROM contacts");
+            ResultSet results = statement.getResultSet();
+            while(results.next()) {
+                System.out.println(results.getString("name") + " " + results.getInt("phone") + " " + results.getString("email"));
+            }
+            results.close();
         } catch (SQLException e) {
             System.out.println("Something went wrong: " + e.getMessage());
         }

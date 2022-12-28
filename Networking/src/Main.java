04d4dfb4ace84e04b0fa3988a7af840ae5673f50
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-28 14:50:12 +0200

    Switches to HttpURLConnection

--- Networking/src/Main.java
+++ Networking/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -11,20 +12,24 @@ public class Main {
     public static void main(String[] args) {
         try {
             URL url = new URL("http://example.org");
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.setDoOutput(true); // only if you want to write to an URL
-            urlConnection.connect();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Chrome");
+            connection.setReadTimeout(30000);
 
-            BufferedReader inputStream = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
-            for (Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
-                String key = entry.getKey();
-                List<String> value = entry.getValue();
-                System.out.println("----Key = " + key);
-                for (String string : value) {
-                    System.out.println("Value = " + string);
-                }
+            int responseConde = connection.getResponseCode();
+            System.out.println("Response code: " + responseConde);
+
+            if (responseConde != 200) {
+                System.out.println("Error reading web page");
+                return;
             }
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            inputReader.close();
         } catch (MalformedURLException e) {
             System.out.println("Malformed URL: " + e.getMessage());
         } catch (IOException e) {

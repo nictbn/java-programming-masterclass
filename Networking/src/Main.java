fd6c0e3ccf408fc17f009c6109fb5ff61747bf97
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-28 14:21:22 +0200

    Reads html from example.org

--- Networking/src/Main.java
+++ Networking/src/Main.java
@@ -1,26 +1,20 @@
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
         try {
             URL url = new URL("http://example.org");
-            URI uri = url.toURI();
-            System.out.println("Scheme = " + uri.getScheme());
-            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-            System.out.println("Authority = " + uri.getAuthority());
-            System.out.println("User info = " + uri.getUserInfo());
-            System.out.println("Host = " + uri.getHost());
-            System.out.println("Port = " + uri.getPort());
-            System.out.println("Path = " + uri.getPath());
-            System.out.println("Query = " + uri.getQuery());
-            System.out.println("Fragment = " + uri.getFragment());
-        } catch (URISyntaxException e) {
-            System.out.println("URI Bad Syntax: " + e.getMessage());
-        } catch (MalformedURLException e) {
-            System.out.println("Malformed URL: " + e.getMessage());
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line = "";
+            while (line != null) {
+                line = inputStream.readLine();
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
         }
     }
 }

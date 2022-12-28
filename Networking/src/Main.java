998cc53778889ee665fa8b502c73c849ddef637d
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-28 14:12:50 +0200

    Multiple relative uris example

--- Networking/src/Main.java
+++ Networking/src/Main.java
@@ -6,11 +6,23 @@ import java.net.URL;
 public class Main {
     public static void main(String[] args) {
         try {
-            URI baseUri = new URI("http://username:password@myserver.com:5000");
-            URI relativeUri = new URI("/catalogue/phones?os=android#samsung");
-            URI resolvedUri = baseUri.resolve(relativeUri);
-            URL url = resolvedUri.toURL();
-            System.out.println("URL = " + url);
+            URI baseUri = new URI("http://username:password@mynewserver.com:5000");
+            URI relativeUri1 = new URI("/catalogue/phones?os=android#samsung");
+            URI relativeUri2 = new URI("/catalogue/tvs?manufacturer=samsung");
+            URI relativeUri3 = new URI("/stores/locations?zip=12345");
+
+            URI resolvedUri1 = baseUri.resolve(relativeUri1);
+            URI resolvedUri2 = baseUri.resolve(relativeUri2);
+            URI resolvedUri3 = baseUri.resolve(relativeUri3);
+
+            URL url1 = resolvedUri1.toURL();
+            System.out.println("URL = " + url1);
+
+            URL url2 = resolvedUri2.toURL();
+            System.out.println("URL = " + url2);
+
+            URL url3 = resolvedUri3.toURL();
+            System.out.println("URL = " + url3);
         } catch (URISyntaxException e) {
             System.out.println("URI Bad Syntax: " + e.getMessage());
         } catch (MalformedURLException e) {

0e02d35a0ebb27763392658996400d3d7131bebf
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-28 13:41:33 +0200

    Base URI + Relative URI Example

--- Networking/src/Main.java
+++ Networking/src/Main.java
@@ -6,9 +6,10 @@ import java.net.URL;
 public class Main {
     public static void main(String[] args) {
         try {
-//            URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
-            URI uri = new URI("/catalogue/phones?os=android#samsung");
-            URL url = uri.toURL();
+            URI baseUri = new URI("http://username:password@myserver.com:5000");
+            URI relativeUri = new URI("/catalogue/phones?os=android#samsung");
+            URI resolvedUri = baseUri.resolve(relativeUri);
+            URL url = resolvedUri.toURL();
             System.out.println("URL = " + url);
         } catch (URISyntaxException e) {
             System.out.println("URI Bad Syntax: " + e.getMessage());

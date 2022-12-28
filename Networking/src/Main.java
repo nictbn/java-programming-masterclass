dcccdaaadea70a5cd34d0be902785fba9d9b648d
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-28 13:32:32 +0200

    Still a valid URI

--- Networking/src/Main.java
+++ Networking/src/Main.java
@@ -4,7 +4,8 @@ import java.net.URISyntaxException;
 public class Main {
     public static void main(String[] args) {
         try {
-            URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+//            URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+            URI uri = new URI("hello");
             System.out.println("Scheme = " + uri.getScheme());
             System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
             System.out.println("Authority = " + uri.getAuthority());

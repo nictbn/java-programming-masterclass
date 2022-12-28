15654d9373c097da24ec041ad2ef23ba0aa93f63
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-28 14:28:11 +0200

    URL Connection example

--- Networking/src/Main.java
+++ Networking/src/Main.java
@@ -1,18 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class Main {
     public static void main(String[] args) {
         try {
             URL url = new URL("http://example.org");
-            BufferedReader inputStream = new BufferedReader(new InputStreamReader(url.openStream()));
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setDoOutput(true); // only if you want to write to an URL
+            urlConnection.connect();
+
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
             String line = "";
             while (line != null) {
                 line = inputStream.readLine();
                 System.out.println(line);
             }
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }

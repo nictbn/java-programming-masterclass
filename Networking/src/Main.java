9529f879f983c275a4be35f2304a69363818bdd1
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-28 13:34:48 +0200

    Converts URI to URL with exception due to unknown protocol

--- Networking/src/Main.java
+++ Networking/src/Main.java
@@ -1,22 +1,17 @@
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
         try {
-//            URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
-            URI uri = new URI("hello");
-            System.out.println("Scheme = " + uri.getScheme());
-            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-            System.out.println("Authority = " + uri.getAuthority());
-            System.out.println("User info = " + uri.getUserInfo());
-            System.out.println("Host = " + uri.getHost());
-            System.out.println("Port = " + uri.getPort());
-            System.out.println("Path = " + uri.getPath());
-            System.out.println("Query = " + uri.getQuery());
-            System.out.println("Fragment = " + uri.getFragment());
+            URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+            URL url = uri.toURL();
         } catch (URISyntaxException e) {
             System.out.println("URI Bad Syntax: " + e.getMessage());
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
         }
     }
 }

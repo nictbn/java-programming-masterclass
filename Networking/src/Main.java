3526f3781309336a9f8eea8456d80506bd8e00ef
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-28 13:31:34 +0200

    URI Example

---
+++ Networking/src/Main.java
@@ -0,0 +1,21 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+            System.out.println("Scheme = " + uri.getScheme());
+            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
+            System.out.println("Authority = " + uri.getAuthority());
+            System.out.println("User info = " + uri.getUserInfo());
+            System.out.println("Host = " + uri.getHost());
+            System.out.println("Port = " + uri.getPort());
+            System.out.println("Path = " + uri.getPath());
+            System.out.println("Query = " + uri.getQuery());
+            System.out.println("Fragment = " + uri.getFragment());
+        } catch (URISyntaxException e) {
+            System.out.println("URI Bad Syntax: " + e.getMessage());
+        }
+    }
+}

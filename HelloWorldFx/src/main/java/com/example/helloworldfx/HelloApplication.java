d50149913e6a1a0c55f9841218d1bd297600bd84
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-29 16:39:44 +0200

    Adds a label to the grid pane

--- HelloWorldFx/src/main/java/com/example/helloworldfx/HelloApplication.java
+++ HelloWorldFx/src/main/java/com/example/helloworldfx/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -17,6 +18,9 @@ public class HelloApplication extends Application {
         root.setAlignment(Pos.CENTER);
         root.setVgap(10);
         root.setHgap(10);
+
+        Label greeting = new Label("Welcome to JavaFx");
+        root.getChildren().add(greeting);
 
         Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello JavaFX!");

4711eedc3778af05376a00b3fd1276510f99592c
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-29 16:45:54 +0200

    Reverts to using FXML

--- HelloWorldFx/src/main/java/com/example/helloworldfx/HelloApplication.java
+++ HelloWorldFx/src/main/java/com/example/helloworldfx/HelloApplication.java
@@ -2,13 +2,7 @@ package com.example.helloworldfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,17 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        GridPane root = new GridPane();
-        root.setAlignment(Pos.CENTER);
-        root.setVgap(10);
-        root.setHgap(10);
 
-        Label greeting = new Label("Welcome to JavaFX!");
-        greeting.setTextFill(Color.GREEN);
-        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
-        root.getChildren().add(greeting);
-
-        Scene scene = new Scene(root, 800, 275);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 275);
         stage.setTitle("Hello JavaFX!");
         stage.setScene(scene);
         stage.show();

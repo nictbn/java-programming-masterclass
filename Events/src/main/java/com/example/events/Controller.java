8f4e4dad98b5418d1a8100ccabc7bd9f8d2126e0
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-31 13:34:07 +0200

    Gets the content of the text field when the button is clicked

--- Events/src/main/java/com/example/events/Controller.java
+++ Events/src/main/java/com/example/events/Controller.java
@@ -1,7 +1,13 @@
 package com.example.events;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField nameField;
+
     public void onButtonClicked() {
-        System.out.println("Hello");
+        System.out.println("Hello, " + nameField.getText());
     }
 }

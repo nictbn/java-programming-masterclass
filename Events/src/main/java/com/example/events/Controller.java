fc296b90c4e100609fb8e6a2bea6e2ae21a09f35
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-31 13:43:35 +0200

    One event handler to handle two controls

--- Events/src/main/java/com/example/events/Controller.java
+++ Events/src/main/java/com/example/events/Controller.java
@@ -2,6 +2,7 @@ package com.example.events;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -9,8 +10,17 @@ public class Controller {
     private TextField nameField;
 
     @FXML
+    private Button helloButton;
+
+    @FXML
+    private Button byeButton;
+
+    @FXML
     public void onButtonClicked(ActionEvent e) {
-        System.out.println("Hello, " + nameField.getText());
-        System.out.println("The following button was pressed: " + e.getSource());
+        if (e.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        } else if (e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
     }
 }

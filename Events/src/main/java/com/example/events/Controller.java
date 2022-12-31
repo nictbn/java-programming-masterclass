34f2a4b9d502d685c67cdf16b269bc1e1504ca5d
Nicu Taban <nictbndeveloper@gmail.com>
2022-12-31 13:50:48 +0200

    DIsables buttons when textfield is empty

--- Events/src/main/java/com/example/events/Controller.java
+++ Events/src/main/java/com/example/events/Controller.java
@@ -16,11 +16,25 @@ public class Controller {
     private Button byeButton;
 
     @FXML
+    public void initialize() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
     public void onButtonClicked(ActionEvent e) {
         if (e.getSource().equals(helloButton)) {
             System.out.println("Hello, " + nameField.getText());
         } else if (e.getSource().equals(byeButton)) {
             System.out.println("Bye, " + nameField.getText());
         }
+    }
+
+    @FXML
+    public void handleKeyReleased() {
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
     }
 }

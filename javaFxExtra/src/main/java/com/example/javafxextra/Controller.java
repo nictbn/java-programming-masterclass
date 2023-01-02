5faa25b57f6c49a2eb3034244020b8489e93576a
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-02 21:04:35 +0200

    Adds Drop Shadow around button four

--- javaFxExtra/src/main/java/com/example/javafxextra/Controller.java
+++ javaFxExtra/src/main/java/com/example/javafxextra/Controller.java
@@ -1,11 +1,20 @@
 package com.example.javafxextra;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 
 public class Controller {
     @FXML
     private Label label;
+
+    @FXML
+    private Button button4;
+
+    public void initialize() {
+        button4.setEffect(new DropShadow());
+    }
 
     @FXML
     public void handleMouseEnter() {

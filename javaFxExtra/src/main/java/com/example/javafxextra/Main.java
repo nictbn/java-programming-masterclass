adfb80922e0d8f868395bf695aec518ff3296b68
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-02 20:55:10 +0200

    Changes Default theme

--- javaFxExtra/src/main/java/com/example/javafxextra/Main.java
+++ javaFxExtra/src/main/java/com/example/javafxextra/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
+        setUserAgentStylesheet(STYLESHEET_CASPIAN);
         Scene scene = new Scene(fxmlLoader.load(), 600, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);

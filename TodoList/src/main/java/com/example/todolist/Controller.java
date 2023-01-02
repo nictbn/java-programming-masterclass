a2cfd5af2f267ddc1d9826536c8a87df7023e088
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-02 12:16:23 +0200

    Prepares mock todo items

--- TodoList/src/main/java/com/example/todolist/Controller.java
+++ TodoList/src/main/java/com/example/todolist/Controller.java
@@ -1,7 +1,34 @@
 package com.example.todolist;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import com.example.todolist.datamodel.TodoItem;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
+    private List<TodoItem> todoItems;
+
+    public void initialize() {
+        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
+                LocalDate.now());
+        TodoItem item2 = new TodoItem("Doctor's Appointment",
+                "See Dr Smith at 123 Main Street. Bring paperwork",
+                LocalDate.now().plusMonths(1));
+        TodoItem item3 = new TodoItem("Finish design proposal for client",
+                "I'd promised Mike I'd email website mockups",
+                LocalDate.now().minusDays(3));
+        TodoItem item4 = new TodoItem("Pickup Doug at the train station",
+                "Doug is arriving",
+                LocalDate.now().minusMonths(1));
+        TodoItem item5 = new TodoItem("Pickup dry cleaning",
+                "The clothes should be ready",
+                LocalDate.now().plusDays(3));
+        todoItems = new ArrayList<>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+    }
 }

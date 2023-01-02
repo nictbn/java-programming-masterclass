3874bc6281efb50b0f8689e0e21b99cadb734479
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-02 21:01:02 +0200

    Zoom in example

--- javaFxExtra/src/main/java/com/example/javafxextra/Controller.java
+++ javaFxExtra/src/main/java/com/example/javafxextra/Controller.java
@@ -4,4 +4,18 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class Controller {
+    @FXML
+    private Label label;
+
+    @FXML
+    public void handleMouseEnter() {
+        label.setScaleX(2.0);
+        label.setScaleY(2.0);
+    }
+
+    @FXML
+    public void handleMouseExit() {
+        label.setScaleX(1.0);
+        label.setScaleY(1.0);
+    }
 }

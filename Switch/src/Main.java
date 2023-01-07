1c026506d8f4b2767d9f507407f3a8d985a0bc00
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-07 10:27:27 +0200

    Switch expression example

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Locale;
-
 public class Main {
     public static void main(String[] args) {
         int value = 1;
@@ -27,6 +25,16 @@ public class Main {
                 break;
             default:
                 System.out.println("was not 1, 2, 3, 4 or 5 or 2");
+        }
+
+        switch (switchValue) {
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
+                System.out.println("Was a 3, a 4 or a 5");
+                System.out.println("Actually it was a " + switchValue);
+            }
+            default -> System.out.println("Was not 1, 2, 3, 4, or 5");
         }
 
         char charValue = 'G';

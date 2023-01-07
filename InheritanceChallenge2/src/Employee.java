b8685c732ac39019641b9d451f1afc6a134a392d
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-07 13:42:48 +0200

    Auto-generates employee id

--- InheritanceChallenge2/src/Employee.java
+++ InheritanceChallenge2/src/Employee.java
@@ -1,10 +1,11 @@
 public class Employee extends Worker {
+    private static int employeeNo = 1;
     private long employeeId;
     private String hireDate;
 
-    public Employee(String name, String birthDate, long employeeId, String hireDate) {
+    public Employee(String name, String birthDate, String hireDate) {
         super(name, birthDate);
-        this.employeeId = employeeId;
+        this.employeeId = Employee.employeeNo++;
         this.hireDate = hireDate;
     }
 

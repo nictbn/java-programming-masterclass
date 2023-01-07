4c0313282d0519a77658d68d4b77e3f47f25cef4
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-07 10:08:12 +0200

    Minor changes

--- ForStatement/src/Main.java
+++ ForStatement/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
-        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 3.0));
-        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 4.0));
-        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 5.0));
+        System.out.println("10,000 at 3% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 4% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0, 5.0));
 
         for (int i = 0; i < 5; i++) {
             System.out.println("Loop " + i + " hello!");
--- ReadingUserInput/src/Main.java
+++ ReadingUserInput/src/Main.java
@@ -1,3 +1,4 @@
+import java.time.Year;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +11,7 @@ public class Main {
             scanner.nextLine(); // handle line terminator;
             System.out.println("Enter your name: ");
             String name = scanner.nextLine();
-            int age = 2022 - yearOfBirth;
+            int age = Year.now().getValue() - yearOfBirth;
             if (age >= 0 && age <= 100) {
                 System.out.println("Your name is " + name + ", and you are " + age + " years old");
             } else {

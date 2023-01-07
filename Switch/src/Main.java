785142ac146c2612c9c4aac0bf0272c646b6dcf6
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-07 10:35:48 +0200

    Traditional switch example

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -63,5 +63,30 @@ public class Main {
             default:
                 System.out.println("Not sure");
         }
+
+        String secondMonth = "OCTOBER";
+        System.out.println(secondMonth + " is in the " + getQuarter(secondMonth) + " quarter");
+    }
+
+    public static String getQuarter(String month) {
+        switch (month) {
+            case "JANUARY":
+            case "FEBRUARY":
+            case "MARCH":
+                return "1st";
+            case "APRIL":
+            case "MAY":
+            case "JUNE":
+                return "2nd";
+            case "JULY":
+            case "AUGUST":
+            case "SEPTEMBER":
+                return "3rd";
+            case "OCTOBER":
+            case "NOVEMBER":
+            case "DECEMBER":
+                return "4th";
+        }
+        return "bad";
     }
 }

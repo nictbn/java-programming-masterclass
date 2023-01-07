77dfa5dcf62e6aba30906799774738bb89b9ee90
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-07 10:53:06 +0200

    Bonus Part Of The Switch Expression Challenge

--- SwitchExpressionChallenge/src/Main.java
+++ SwitchExpressionChallenge/src/Main.java
@@ -3,6 +3,10 @@ public class Main {
         for (int i = 0; i <= 7; i++) {
             printDayOfWeek(i);
         }
+        System.out.println("*** Bonus Challenge ***");
+        for (int i = 0; i <= 7; i++) {
+            printWeekDay(i);
+        }
     }
 
     public static void printDayOfWeek(int day) {
@@ -16,6 +20,26 @@ public class Main {
             case 6 -> "Saturday";
             default -> "Invalid Day";
         };
+        System.out.println(day + " stands for " + dayOfWeek);
+    }
+
+    public static void printWeekDay(int day) {
+        String dayOfWeek = "Invalid Day";
+        if (day == 0) {
+            dayOfWeek = "Sunday";
+        } else if (day == 1) {
+            dayOfWeek = "Monday";
+        } else if (day == 2) {
+            dayOfWeek = "Tuesday";
+        } else if (day == 3) {
+            dayOfWeek = "Wednesday";
+        } else if (day == 4) {
+            dayOfWeek = "Thursday";
+        } else if (day == 5) {
+            dayOfWeek = "Friday";
+        } else if (day == 6) {
+            dayOfWeek = "Saturday";
+        }
         System.out.println(day + " stands for " + dayOfWeek);
     }
 }

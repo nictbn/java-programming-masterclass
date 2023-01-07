a6ee8711774e3194dcfaff93fe5409facf42b7c0
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-07 11:40:10 +0200

    Introduction to the Record Type

--- RecordProject/src/Main.java
+++ RecordProject/src/Main.java
@@ -9,6 +9,27 @@ public class Main {
             );
             System.out.println(s);
         }
+        System.out.println("******* Record Example *******");
+        for (int i = 0; i < 5; i++) {
+            LPAStudent s = new LPAStudent(
+                    "S92300" + i,
+                    getName(i),
+                    "05/11/1985",
+                    "Java Masterclass"
+            );
+            System.out.println(s);
+        }
+        System.out.println();
+        Student pojoStudent = new Student("S923006", "Ann", "05/11/1985", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("S923007", "Bill", "05/11/1985", "Java Masterclass");
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+        // Records are immutable
+        
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
     }
     public static String getName(int i) {
         return switch (i) {

5db49699ee6f92e4637d321d0e6c81fbc5ebfe27
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-08 12:57:20 +0200

    Composition Challenge without getters

--- CompositionChallenge/src/SmartKitchen.java
+++ CompositionChallenge/src/SmartKitchen.java
@@ -1,12 +1,24 @@
 public class SmartKitchen {
-    private CoffeeMaker brewMaster;
-    private Refrigerator iceBox;
-    private DishWasher dishwasher;
+    private final CoffeeMaker brewMaster;
+    private final Refrigerator iceBox;
+    private  final DishWasher dishwasher;
 
     public SmartKitchen() {
         brewMaster = new CoffeeMaker();
         iceBox = new Refrigerator();
         dishwasher = new DishWasher();
+    }
+
+    public void setKitchenState(boolean coffeeMachineHasWork, boolean fridgeHasWork, boolean dishWasherHasWork) {
+        brewMaster.setHasWorkToDo(coffeeMachineHasWork);
+        iceBox.setHasWorkToDo(fridgeHasWork);
+        dishwasher.setHasWorkToDo(dishWasherHasWork);
+    }
+
+    public void doKitchenWork() {
+        brewMaster.brewCoffee();
+        iceBox.orderFood();
+        dishwasher.doDishes();
     }
 
     public CoffeeMaker getBrewMaster() {
